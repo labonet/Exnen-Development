bed5791d1aee5b0759601dff60061a2fb7b860d2
Bulcane <93993348+Bulcanolol@users.noreply.github.com>
2023-02-15 21:36:30 +0100

    Delete commands directory

--- commands/version.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    name: "version",
-    description: "view exnen current version",
-    usage: "version\nOptional: --last",
-    execute() {
-        console.log("1.0.0")
-    }
-}
